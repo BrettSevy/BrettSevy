86fb73ca3b00d33216c3a952626a93ae4ceb3493
Brett Sevy <justbrettt@gmail.com>
2020-04-06 20:05:15 -0500

    finishedh push

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>
+        <a id="jumboHeader" href="/">
+        Reading List
+        </a>
+        </h1>
+    </div>
+  );
+}
+
+export default Jumbotron;
